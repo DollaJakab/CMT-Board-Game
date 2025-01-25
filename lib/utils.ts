57787d5f4a9c8875e62fc9ceef0a2fd06cc440e1
DollaJakab <jakab.dolla@gmail.com>
2025-01-25 20:25:39 +0100

    Update metadata for CMT Board Game and clean up unused console logs

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,8 +32,6 @@ export const linesCalculator = (board: CellProps[][]) => {
         for (let j = 0; j < cols; j++) {
             const letter = board[i][j].value;
             if (!letter) continue;
-            console.log('letter', letter);
-
             const directions = [
                 [0, 1],
                 [1, 0],
@@ -60,6 +58,66 @@ export const linesCalculator = (board: CellProps[][]) => {
             processedKey.includes(key) && processedKey !== key
         );
     });
+    console.log('filteredResults', filteredResults);
     return filteredResults;
 }
 
+// export const linesCalculator = (board: CellProps[][]) => {
+//     const lines = []
+
+//     const countHorizontalLines = () => {
+//         const linesHorizontal = [];
+//         board.forEach((row, rowIndex) => {
+//             let currentLetter = row[0].value;
+//             let length = 0;
+//             row.forEach((cell, colIndex) => {
+//                 if (cell.value === currentLetter && cell.value) {
+//                     length++;
+//                 } else {
+//                     if (length >= 3) {
+//                         linesHorizontal.push(new Array(length).fill(0).map((_, i) => [rowIndex, colIndex - i - 1]));
+//                     }
+//                     length = 0;
+//                     currentLetter = cell.value;
+//                 }
+//                 if (length >= 3 && colIndex === row.length - 1) {
+//                     linesHorizontal.push(new Array(length).fill(0).map((_, i) => [rowIndex, row.length - i - 1]));
+//                 }
+//             });
+//         });
+//         return linesHorizontal;
+//     }
+
+//     const countVerticalLines = () => {
+//         const linesVertical = [];
+//         for (let colIndex = 0; colIndex < board[0].length; colIndex++) {
+//             let currentLetter = board[0][colIndex].value;
+//             let length = 0;
+//             for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+//                 const cell = board[rowIndex][colIndex];
+//                 if (cell.value === currentLetter && cell.value) {
+//                     length++;
+//                 } else {
+//                     if (length >= 3) {
+//                         linesVertical.push(new Array(length).fill(0).map((_, i) => [rowIndex - i - 1, colIndex]));
+//                     }
+//                     length = 0;
+//                     currentLetter = cell.value;
+//                 }
+//                 if (length >= 3 && rowIndex === board.length - 1) {
+//                     linesVertical.push(new Array(length).fill(0).map((_, i) => [rowIndex - i, colIndex]));
+//                 }
+//             }
+//         }
+//         return linesVertical;
+//     }
+
+//     const horizontalLines = countHorizontalLines();
+//     const verticalLines = countVerticalLines();
+
+//     lines.push(...horizontalLines);
+//     lines.push(...verticalLines);
+
+//     console.log('lines', lines);
+//     return lines;
+// }
